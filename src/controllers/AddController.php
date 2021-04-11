0829d16a02033b1741bfa09bb18edec8b7e91003
Vincent <groner04@gmail.com>
2021-04-11 15:58:04 +0200

    -

--- src/controllers/AddController.php
+++ src/controllers/AddController.php
@@ -8,8 +8,6 @@ if (isset($_POST['submit'])) {
 	$tempname = $_FILES["uploadfile"]["tmp_name"];
 	$folder = "../report-img/".$filename;
 
-    print $_POST["title"]. $_POST["comment"]. $_POST["lat"]. $_POST["lng"]. $filename;
-
 	$model->addReportToDB($_POST["title"], $_POST["comment"], $_POST["lat"], $_POST["lng"], $filename);
 
 	move_uploaded_file($tempname, $folder);
