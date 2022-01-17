1adfccb908239c16d0762044bba4f6d29e4bd2b7
Vincent <groner04@gmail.com>
2022-01-17 10:50:56 +0100

    fixed update status without rights

--- src/controllers/UpdateStatusController.php
+++ src/controllers/UpdateStatusController.php
@@ -1,8 +1,16 @@
 <?php
 require_once("../models/ReportModel.php");
+require_once("../models/UserModel.php");
+session_start();
 
 $model = new ReportModel;
+$userModel = new UserModel();
 if (isset($_GET["value"]) && isset($_GET["id"])){
-    $model->updateStatus($_GET["value"], $_GET["id"]);
-    header("Location: ../views/showReport.html?id=".$_GET["id"]);
+    $user = $userModel->getUserByEmail($_SESSION["email"]);
+    $report = $model->getReportById($_GET["id"]);
+
+    if ($report->User_fk == $user->User_id) {
+        $model->updateStatus($_GET["value"], $_GET["id"]);
+        header("Location: ../views/showReport.html?id=" . $_GET["id"]);
+    }
 }
